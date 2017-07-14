b7221e1e287c465bbe5176f2906ab0caaf393ca2
Alain Mosnier <alain@wanamoon.net>
2017-07-14 15:22:48 +0200

    Updated code

--- extra_code/12_ball_racket.py
+++ extra_code/12_ball_racket.py
@@ -1,4 +1,5 @@
 import tkinter
+from enum import Enum, auto
 
 class Ball:
     def __init__(self, canvas, x, y, size, color, step_size):
@@ -34,6 +35,42 @@ class Ball:
     def top_border_reached(self, coords):
         return coords[1] < 0
 
+class Racket:
+    class Direction(Enum):
+        NONE = auto()
+        LEFT = auto()
+        RIGHT = auto()
+
+    def __init__(self, canvas, x, y, x_size, y_size, color, step_size):
+        self.canvas = canvas
+        self.step_size = step_size
+        self.id = canvas.create_rectangle(x, y, x + x_size, y + y_size, fill=color)
+        self.canvas_width = self.canvas.winfo_width()
+        self.direction = Racket.Direction.NONE
+        
+        canvas.bind_all('<KeyPress-Left>', self.on_left_pressed)
+        canvas.bind_all('<KeyPress-Right>', self.on_right_pressed)
+        canvas.bind_all('<KeyRelease-Left>', self.on_left_released)
+        canvas.bind_all('<KeyRelease-Right>', self.on_right_released)
+        
+    def on_left_pressed(self, event):
+        self.direction = Racket.Direction.LEFT
+
+    def on_right_pressed(self, event):
+        self.direction = Racket.Direction.RIGHT
+
+    def on_left_released(self, event):
+        if self.direction == Racket.Direction.LEFT:
+            self.direction = Racket.Direction.NONE
+        
+    def on_right_released(self, event):
+        if self.direction == Racket.Direction.RIGHT:
+            self.direction = Racket.Direction.NONE
+
+    def move(self):
+        dx = 0
+        
+
 tk = tkinter.Tk()
 
 tk.title("Bounce")
@@ -50,6 +87,7 @@ canvas.pack()
 tk.update() # necessary for winfo_xxx functions to work
 
 ball = Ball(canvas, 100, 100, 15, 'coral', 5)
+racket = Racket(canvas, 250, 370, 80, 10, 'lightgreen', 3)
 
 def handle_timer_event():
     ball.move()
